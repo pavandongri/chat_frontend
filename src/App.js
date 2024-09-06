2cd4ea294e0eb412aeeb3d63b738b77c03c3c353
Dongri  Pavan Kumar <dongri.pavan@jithvar.com>
2024-09-06 21:30:11 +0530

    commit - 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import Header from "./components/Header"
+
+import io from 'socket.io-client';
+const socket = io('http://localhost:3005');
+const { v4: uuidv4 } = require('uuid');
+
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const name = uuidv4()
+    setUsername(name)
+
+    socket.on('chat message', (msg) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+
+    return () => {
+      socket.off('chat message');
+    };
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    const msgObj = { text: message, sender: username };
+    if (message) {
+      socket.emit('chat message', msgObj);
+    }
+    setMessage('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+
+      <div className="chat-container">
+        {messages.map((msg, index) => (
+          <div className={msg.sender === username ? 'msg-sent' : 'msg-received'} key={index}>
+            <p >{msg.text}</p>
+          </div>
+        ))}
+      </div>
+
+      <form className='msg-form' onSubmit={sendMessage}>
+        <div className='msg-input-container'>
+          <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Type a message..."
+          />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+
+
     </div>
   );
 }
